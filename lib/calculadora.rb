b7fcd5b09236823d01b4dbc9547a917590ebd4ea
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-10-26 19:49:50 -0300

    Desafio Calculator Ruby

--- lib/calculadora.rb
+++ lib/calculadora.rb
@@ -1,8 +1,20 @@
 def soma(primeiro_numero, segundo_numero)
+    resultado = primeiro_numero + segundo_numero
 end
 
 def subtracao(primeiro_numero, segundo_numero)
+    resultado = primeiro_numero - segundo_numero
 end
 
-def multiplicacao
+def multiplicacao(primeiro_numero,segundo_numero)
+    resultado = primeiro_numero * segundo_numero
+end
+
+def divisao(primeiro_numero,segundo_numero)
+    if segundo_numero == 0
+    resultado = 'Opa! Zero como divisor'
+    else
+    resultado = primeiro_numero / segundo_numero
+    end
+    resultado
 end
